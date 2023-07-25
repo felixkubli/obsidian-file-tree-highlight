60a093de2d16ac7db51f6d8c159b8d8d4a842953
Felix Kubli <felixkubli@bluewin.ch>
2023-07-25 23:22:14 +0200

    add color editor modal

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -16,66 +16,49 @@ export default class FileTreeHighlight extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Greet', (_: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('Hello Obsidian, this is the File Tree Highlighter Plugin!!!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'open-sample-modal-color-setter',
+			name: 'edit background color',
 			callback: () => {
-				new SampleModal(this.app).open();
+				new ColorEditorModal(this.app, opts => console.log(opts)).open();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, _: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		this.registerEvent(
+			this.app.workspace.on("file-menu", (menu, file) => {
+				console.log(file);
+				menu.addItem((item) => {
+					item
+						.setTitle("edit highlighting options")
+						.setIcon("palette")
+						.onClick(async () => {
+							new ColorEditorModal(this.app, opts => console.log(opts)).open();
+					});
+				});
+			})
+		);
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		// This adds a complex command that can check whether the current state of the app allows execution of the command
+		// this.addCommand({
+		// 	id: 'open-sample-modal-complex',
+		// 	name: 'Open sample modal (complex)',
+		// 	checkCallback: (checking: boolean) => {
+		// 		// Conditions to check
+		// 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		// 		if (markdownView) {
+		// 			// If checking is true, we're simply "checking" if the command can be run.
+		// 			// If checking is false, then we want to actually perform the operation.
+		// 			if (!checking) {
+		// 				new SampleModal(this.app).open();
+		// 			}
+
+		// 			// This command will only show up in Command Palette when the check function returns true
+		// 			return true;
+		// 		}
+		// 	}
+		// });
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new FileTreeHighlightSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -91,14 +74,52 @@ export default class FileTreeHighlight extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+interface HighlightingOption {
+	backgroundColor: string
+}
 
+class ColorEditorModal extends Modal {
+	result: HighlightingOption
+	onSubmit: (result?: HighlightingOption) => void
+
+	constructor(app: App, onSubmit: (result?: HighlightingOption) => void) {
+		super(app);
+		this.result = { backgroundColor: '' };
+		this.onSubmit = onSubmit;
+	}
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		contentEl.setText('Edit the background color');
+		contentEl.createEl("h1", { text: "What's your name?" });
+
+		new Setting(contentEl)
+			.setName("Background Color")
+			.addText((text) =>
+				text.onChange((value: string) => {
+					this.result.backgroundColor = value;
+				})
+			);
+
+		new Setting(contentEl)
+			.addButton(btn =>
+				btn
+				  .setButtonText("Remove Element Settings")
+				  .setCta()
+				  .onClick(() => {
+					this.close();
+					this.onSubmit(undefined);
+				  })
+			)
+			.addButton(btn =>
+				btn
+				  .setButtonText("Submit")
+				  .setCta()
+				  .onClick(() => {
+					this.close();
+					this.onSubmit(this.result);
+				  })
+			);
+
 	}
 
 	onClose() {
